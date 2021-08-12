ec9509768f6fe58bc88a3783d664b8c8ade834f6
Ekaterina <40368278+klassikate@users.noreply.github.com>
2021-08-12 13:41:25 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+ import $ from 'jquery'
+ window.jQuery = $
+ window.$ = $
+
+// // Import vendor jQuery plugin example (not module)
+// require('~/app/libs/mmenu/dist/mmenu.js')
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	
+
+})
